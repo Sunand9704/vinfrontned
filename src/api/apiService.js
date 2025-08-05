d3572797ab8a1aaa4347b7c938de614e18c2489b
Dhanush9866 <mahidhanush20@gmail.com>
2025-08-05 12:18:23 +0530

    link update

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // Create a more resilient axios instance
 const api = axios.create({
   baseURL:
-    "http://localhost:8081/api" ||
     "https://back3-728k.onrender.com/api" ||
     import.meta.env.VITE_API_URL ||
     "https://vin2grow.in/api",
